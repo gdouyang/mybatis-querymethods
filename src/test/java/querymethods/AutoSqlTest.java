1cd18fb1ffb6956734ad64aaee88ffb797be9cd8
有理想的鸭子 <578541570@qq.com>
2020-04-22 16:06:31 +0800

    单元测试

--- src/test/java/querymethods/AutoSqlTest.java
+++ src/test/java/querymethods/AutoSqlTest.java
@@ -1,85 +1,43 @@
 package querymethods;
 
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
-
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.entity.Config;
-import tk.mybatis.mapper.mapperhelper.MapperHelper;
 
 /**
  * 
  * @author OYGD
  *
  */
-public class AutoSqlTest
-{
+public class AutoSqlTest extends BaseTest {
 	@Test
 	public void test() {
-		String resource = "mybatis-config.xml";
-		SqlSessionFactory sqlSessionFactory = null;
-		try {
-			InputStream inputStream = Resources.getResourceAsStream(resource);
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		SqlSession session = sqlSessionFactory.openSession();
-		
-		//创建一个MapperHelper
-		MapperHelper mapperHelper = new MapperHelper();
-		//特殊配置
-		Config config = new Config();
-		//主键自增回写方法,默认值MYSQL,详细说明请看文档
-		config.setIDENTITY("MYSQL");
-		//支持getter和setter方法上的注解
-		config.setEnableMethodAnnotation(true);
-		//设置 insert 和 update 中，是否判断字符串类型!=''
-		config.setNotEmpty(true);
-		//校验Example中的类型和最终调用时Mapper的泛型是否一致
-		config.setCheckExampleEntityClass(true);
-		//启用简单类型
-		config.setUseSimpleType(true);
-		//枚举按简单类型处理
-		config.setEnumAsSimpleType(true);
-		//自动处理关键字 - mysql
-		config.setWrapKeyword("`{0}`");
-		//设置配置
-		mapperHelper.setConfig(config);
-		//注册通用接口，和其他集成方式中的 mappers 参数作用相同
-		//4.0 之后的版本，如果类似 Mapper.class 这样的基础接口带有 @RegisterMapper 注解，就不必在这里注册
-		mapperHelper.registerMapper(Mapper.class);
-		mapperHelper.processConfiguration(session.getConfiguration());
-		QueryMethodsHelper.processConfiguration(session.getConfiguration(), mapperHelper);
-				
+
 		try {
-		  BlogMapper mapper = session.getMapper(BlogMapper.class);
-		  
-//		  insert(mapper);
-		  
-		  Blog blog = mapper.findByIdOrFirstName(1, "OY1");
-		  System.out.println(blog);
-		  
-		  Integer countById = mapper.countById(1);
-		  System.out.println(countById);
+			BlogMapper mapper = session.getMapper(BlogMapper.class);
+
+			Integer id = 1;
+			mapper.deleteByPrimaryKey(id);
+			
+			Blog b = new Blog();
+			b.setId(id);
+			b.setFirstName("OY");
+			b.setLastName("GD");
+			mapper.insert1(b);
+
+			Blog blog = mapper.findByIdOrFirstName(id, "OY1");
+			assert blog != null;
+
+			Integer countById = mapper.countById(id);
+			assert countById != null;
+			
+			blog = mapper.findByIdAndFirstName(b.getId(), b.getFirstName());
+			assert blog != null; 
+			
 		} finally {
 			session.commit();
-		  session.close();
+			session.close();
 		}
 	}
-	
-	static void insert(BlogMapper mapper) {
-		Blog b = new Blog();
-		  b.setId(1);
-		  b.setFirstName("OY");
-		  b.setLastName("GD");
-		  mapper.insert1(b);
-	}
+
 }
