c49dc0d390540e9f1fd4d459af3fae92fd7fabe5
有理想人鸭子 <578541570@qq.com>
2020-07-15 21:48:52 +0800

    #bugfix 修复查询参数为null时查全表问题

--- src/main/java/querymethods/util/ExampleUtil.java
+++ src/main/java/querymethods/util/ExampleUtil.java
@@ -2,11 +2,15 @@ package querymethods.util;
 
 import java.util.Queue;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import querymethods.springdata.PartTreeFactory;
 import querymethods.springdata.query.domain.Sort;
 import querymethods.springdata.query.parser.Part;
 import querymethods.springdata.query.parser.PartTree;
 import querymethods.springdata.query.parser.PartTree.OrPart;
+import tk.mybatis.mapper.MapperException;
 import tk.mybatis.mapper.entity.Example;
 import tk.mybatis.mapper.entity.Example.OrderBy;
 import tk.mybatis.mapper.util.StringUtil;
@@ -18,6 +22,8 @@ import tk.mybatis.mapper.util.StringUtil;
  *
  */
 public class ExampleUtil {
+
+  protected final static Log logger = LogFactory.getLog(ExampleUtil.class);
 
   private ExampleUtil() {}
 
@@ -37,18 +43,22 @@ public class ExampleUtil {
     String methodName = MsIdUtil.getMethodName(msId);
 
     PartTree tree = PartTreeFactory.create(msId, methodName);
-    Example example = new Example(entityClass);
+    Example example = new Example(entityClass, true, true);
     example.setDistinct(tree.isDistinct());
     if (StringUtil.isNotEmpty(tree.getQueryProperty())) {
       example.selectProperties(tree.getQueryProperty());
     }
-    Example.Criteria criteria = example.createCriteria();
-    for (OrPart node : tree) {
-      for (Part part : node) {
-        WhereBuilder.build(part, criteria, params);
+    try {
+      Example.Criteria criteria = example.createCriteria();
+      for (OrPart node : tree) {
+        for (Part part : node) {
+          WhereBuilder.build(part, criteria, params);
+        }
+        criteria = example.createCriteria();
+        example.or(criteria);
       }
-      criteria = example.createCriteria();
-      example.or(criteria);
+    } catch (MapperException e) {
+      throw new MapperException(e.getMessage() + " -> " + msId, e);
     }
     Sort sort = tree.getSort();
     if (sort != null) {
